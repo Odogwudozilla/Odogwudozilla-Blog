e2bf8a615551461d8ed1ae1fa79a16402d6a8d1b
Chidozie Nnachor <odogwu.naija@gmail.com>
2018-06-15 03:29:35 +0100

    migrations deleted

--- db/migrate/20180612235341_add_omniauth_to_odogwus.rb
+++
@@ -1,8 +0,0 @@
-class AddOmniauthToOdogwus < ActiveRecord::Migration[5.1]
-  def change
-    add_column :odogwus, :provider, :string
-    add_index :odogwus, :provider
-    add_column :odogwus, :uid, :string
-    add_index :odogwus, :uid
-  end
-end
