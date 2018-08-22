15ee4603fabb7f8fb8e52c4a0942ad9f04ec98cd
Chidozie Nnachor <odogwu.naija@gmail.com>
2018-08-22 04:41:31 +0100

    Custom routes and hover on icons

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   end
   root to: 'static_pages#home'
 
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/projects'
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages/contact'
+  get 'projects', to: 'static_pages#projects'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
