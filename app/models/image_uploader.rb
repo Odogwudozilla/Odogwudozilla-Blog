8e037c72bbf07aa64428fa92d09e55425eeb4ca6
Chidozie Nnachor <odogwu.naija@gmail.com>
2018-09-16 14:59:13 +0100

    Fixing image upload in trix (size issue)

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < Shrine
 
 
   Attacher.validate do
-    validate_max_size 1.megabyte, message: "is too large (max is 1 MB)"
+    validate_max_size 10.megabyte, message: "is too large (max is 10 MB)"
     validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png']
   end
 end
